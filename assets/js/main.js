ab395d94b0beacad10dd06458542b2e8e78c4280
Nestorahumadag <nestor.a2013@hotmail.com>
2024-03-27 12:39:37 -0300

    creacion de formulario

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+
+$(function(){
+
+    $("#enviarmensaje").click (function(){
+        alert("El mensaje fue enviado correctamente")
+    });
+});
